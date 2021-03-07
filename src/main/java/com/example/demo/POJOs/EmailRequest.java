54a34519ea124a613ee79998e9821f3e753dfa1d
orcun.bassimsek <bassimsek.orcun@metu.edu.tr>
2021-03-07 22:09:04 +0300

    some needed POJOs

---
+++ src/main/java/com/example/demo/POJOs/EmailRequest.java
@@ -0,0 +1,36 @@
+package com.example.demo.POJOs;
+
+import java.io.Serializable;
+
+public class EmailRequest implements Serializable {
+
+    private static final long serialVersionUID = 1505586152401995790L;
+
+    private String toMail;
+    private String date;
+
+    public EmailRequest() {
+    }
+
+
+    public EmailRequest(String toMail, String amount, String date) {
+        this.toMail = toMail;
+        this.date = date;
+    }
+
+    public String getToMail() {
+        return toMail;
+    }
+
+    public void setToMail(String toMail) {
+        this.toMail = toMail;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/com/example/demo/POJOs/UsernameAndPasswordAuthenticationRequest.java
@@ -0,0 +1,30 @@
+package com.example.demo.POJOs;
+
+import java.io.Serializable;
+
+public class UsernameAndPasswordAuthenticationRequest implements Serializable {
+
+    private static final long serialVersionUID = 963436562789986344L;
+
+    private String username;
+    private String password;
+
+    public UsernameAndPasswordAuthenticationRequest() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
